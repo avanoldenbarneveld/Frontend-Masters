703eafc6e7f2bf34f997d9209c6535fa8375da7b
Alberto van Oldenbarneveld <albertovanoldenbarneveld@gmail.com>
2025-11-11 09:30:14 +0100

    solve the three pillars exercise

--- getting-into-javascript/exercises/three-pillars/ex.js
+++ getting-into-javascript/exercises/three-pillars/ex.js
@@ -1,27 +1,32 @@
 class Bookshelf {
 	constructor() {
-		this.favoriteBooks = [];
+		this.favoriteBooks = []
+	}
+
+	addFavoriteBook(bookName) {
+		if (!bookName.includes("Great")) {
+			this.favoriteBooks.push(bookName);
+		}
+	}
+
+	printFavoriteBooks() {
+		console.log("Favorite Books: " + String(this.favoriteBooks.length));
+		for (let bookName of this.favoriteBooks) {
+			console.log(bookName);
+		}
 	}
 
 	// TODO: define methods `addFavoriteBook(..)`
 	// and `printFavoriteBooks()`
 }
 
-function addFavoriteBook(bookName) {
-	if (!bookName.includes("Great")) {
-		favoriteBooks.push(bookName);
-	}
-}
-
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
-
-function loadBooks( /* .. */ ) {
-	// TODO: call fakeAjax( .. );
+function loadBooks(bookshelf) {
+	fakeAjax(BOOK_API, function (books) {
+		for (let bookName of books) {
+			bookshelf.addFavoriteBook(bookName);
+		}
+		bookshelf.printFavoriteBooks();
+	});
 }
 
 var BOOK_API = "https://some.url/api";
@@ -30,8 +35,8 @@ var BOOK_API = "https://some.url/api";
 // ***********************
 
 // NOTE: don't modify this function at all
-function fakeAjax(url,cb) {
-	setTimeout(function fakeLoadingDelay(){
+function fakeAjax(url, cb) {
+	setTimeout(function fakeLoadingDelay() {
 		cb([
 			"A Song of Ice and Fire",
 			"The Great Gatsby",
@@ -39,5 +44,8 @@ function fakeAjax(url,cb) {
 			"Great Expectations",
 			"You Don't Know JS"
 		]);
-	},500);
+	}, 500);
 }
+
+var myBooks = new Bookshelf();
+loadBooks(myBooks);
